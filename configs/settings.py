fcd130c56ab45b767e0137170429ae11c276ff36
Mehdi Mohseni <wuka.sec@gmail.com>
2025-11-15 12:06:34 -0500

    Fix: debig -> false

--- configs/settings.py
+++ configs/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3uvq#r7c51llpcygrmz%sp&%ts5g-vu0b+!cyps@)x611z_^e7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
